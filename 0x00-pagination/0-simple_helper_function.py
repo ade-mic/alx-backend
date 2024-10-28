405acaf3ccaf3786080f9c9cc4ed4530ad3b4cd4
ade-mic <ademic.aina@gmail.com>
2024-10-28 11:12:21 +0100

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,14 +6,15 @@ page and page_size.
 from typing import Tuple
 
 
-def index_range(page:int, page_size:int) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple:
     """
     Indexed a list for pagination.
     Args:
         page(int): the page to be displayed
         page_size(int): the number of item in the page
     Returns:
-        (start_index, end_index): a tuple containing the start index and end index
+        (start_index, end_index): a tuple containing the start index
+        and end index
     """
     start = (page - 1) * page_size
     end = start + page_size
