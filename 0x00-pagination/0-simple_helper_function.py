48e4fb29e257c4f637f35d9fe0bf2c3dc3c69c4d
ade-mic <ademic.aina@gmail.com>
2024-10-28 11:06:42 +0100

    first commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+The module contain function index_range  two integer arguments
+page and page_size.
+"""
+from typing import Tuple
+
+
+def index_range(page:int, page_size:int) -> Tuple:
+    """
+    Indexed a list for pagination.
+    Args:
+        page(int): the page to be displayed
+        page_size(int): the number of item in the page
+    Returns:
+        (start_index, end_index): a tuple containing the start index and end index
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
