ccf989d1a59f79e12839153d1b08114172526a22
ade-mic <ademic.aina@gmail.com>
2024-11-05 17:05:14 +0100

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,52 +1,37 @@
 #!/usr/bin/env python3
 """
-setup a basic Flask app in 0-app.py.
-Create a single / route and an index.html template
-that simply outputs “Welcome to Holberton” as page title (<title>)
-and “Hello world” as header (<h1>).
+Use the _ or gettext function to parametrize your templates.
+Use the message IDs home_title and home_header.
 """
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_babel import Babel, _
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
-
-
 class Config:
     """
-     Holds the configuration for
-     languages (LANGUAGES), default locale
-     """
+    Configuration class with supported
+    languages
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app.config.from_object(Config)
+
 babel = Babel(app)
-
-
-@babel.localeselector
-def get_locale():
-    """
-    get_locale function with the babel.localeselector decorator.
-    Use request.accept_languages to
-    determine the best match with our supported languages.
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     """
-    Create a single / route and an index.html template
-    that simply outputs “Welcome to Holberton” as page title (<title>)
-    and “Hello world” as header (<h1>).
+    Use the _ or gettext function to parametrize your templates.
+    Use the message IDs home_title and home_header.
     """
     return render_template('3-index.html',
-                           home_title=_('home_header'),
-                           home_header=_('home_title')
-                            )
+                           home_title=_('home_title'),
+                           home_header=_('home_header'))
 
 
 if __name__ == '__main__':
