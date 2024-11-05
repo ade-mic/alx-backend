3fa82387bd888e83d9b7d88138f9816bfb2688b0
ade-mic <ademic.aina@gmail.com>
2024-11-05 18:45:23 +0100

    5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -3,11 +3,16 @@
 Use the _ or gettext function to parametrize your templates.
 Use the message IDs home_title and home_header.
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
 app = Flask(__name__)
-
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config:
@@ -36,6 +41,23 @@ def get_locale():
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+def get_user():
+    """
+    Create a function to get the user based
+    on the login_as parameter:
+    """
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users[int(user_id)]
+    return None
+
+@app.before_request
+def before_request():
+    """Use the before_request decorator
+    to set the user before handling the request
+    """
+    g.user = get_user()
 
 
 @app.route('/')
