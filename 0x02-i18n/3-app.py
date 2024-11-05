25cd37c51fdc5465ab85c523445df68a2f617681
ade-mic <ademic.aina@gmail.com>
2024-11-05 16:01:04 +0100

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+setup a basic Flask app in 0-app.py.
+Create a single / route and an index.html template
+that simply outputs “Welcome to Holberton” as page title (<title>)
+and “Hello world” as header (<h1>).
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+     Holds the configuration for
+     languages (LANGUAGES), default locale
+     """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get_locale function with the babel.localeselector decorator.
+    Use request.accept_languages to
+    determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    Create a single / route and an index.html template
+    that simply outputs “Welcome to Holberton” as page title (<title>)
+    and “Hello world” as header (<h1>).
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
